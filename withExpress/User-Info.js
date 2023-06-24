b106cd8ba5936b89dbd38ca1f58a10dfb37ce9f3
Shiny <shinymary.chandran@crystaldelta.com>
2023-06-24 22:00:42 +0530

    Task-2-Changes

--- withExpress/User-Info.js
+++ withExpress/User-Info.js
@@ -1,18 +1,60 @@
-const express = require("express")
+const express = require("express");
 
 const app = express();
 
-const port = process.env.PORT || 8080;
+app.use(express.json());
 
-app.get('/',(req,res)=>{
-    res.send("hello");
+const port = process.env.PORT || 3000;
 
-})
+const data = [
+  {
+    Name: "Shiny",
+    id: 1,
+    city: "Coimbatore",
+  },
+  {
+    Name: "Mary",
+    id: 2,
+    city: "Chennai",
+  },
+];
 
-app.get('/home',(req,res)=>{
-    res.send("Welcome")
-})
+app.get("/", (req, res) => {
+  res.send("Hello,Welcome");
+});
 
-app.listen(port, ()=>{
-    console.log("Running on port",port);
-})
+app.get("/home", (req, res) => {
+  res.send(data);
+});
+
+app.get("/home/:id", (req, res) => {
+  let body = data.find((s) => s.id === parseInt(req.params.id));
+  res.send(body);
+});
+
+app.put("/home/:id", (req, res) => {
+  let body = data.find((s) => s.id === parseInt(req.params.id));
+  res.send(body);
+});
+
+app.post("/home", (req, res) => {
+  const bodyData = {
+    Name: req.body.Name,
+    id: data.length + 1,
+    city: req.body.city,
+  };
+
+  data.push(bodyData);
+  res.send(data);
+});
+
+app.delete("/home/:id", (req, res) => {
+  let body = data.find((s) => s.id === parseInt(req.params.id));
+  const dataDelete = data.indexOf(body);
+  data.splice(dataDelete, 1);
+  res.send(data);
+});
+
+app.listen(port, () => {
+  console.log("Running on port", port);
+});
