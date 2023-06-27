e4fa8d656adede8241c340fe23b604bee9f80ffa
Shiny <shinymary.chandran@crystaldelta.com>
2023-06-27 13:13:14 +0530

    Task-3

--- Task-3/src/app.test.ts
+++ Task-3/src/app.test.ts
@@ -1,5 +1,4 @@
-const { myDataSource } = require("./data-connection");
-const { User } = require("./User");
+
 const app = require("./app");
 
 describe("getting the value from the user",() =>{
@@ -14,9 +13,7 @@ describe("getting the value from the user",() =>{
       Country:"India"
     }]
     const response =  request(app).get("/app");
-    const spy = jest.spyOn( myDataSource.getRepository(User)).mockReturnValue(list);
     expect(response.body).toEqual(list );
-    expect(spy).toHaveBeenCalledTimes(1);
     expect(response.body).toHaveLength(7);
   })
 })
--- Task-3/src/app.ts
+++ Task-3/src/app.ts
@@ -5,7 +5,7 @@ import { User } from "./User";
 const express = require("express");
 const app = express();
 
-const port = 3000;
+const port = 3003;
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(bodyParser.json());
